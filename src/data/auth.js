9fb1bb4ad4b85aadba41f2ff26450e1ecb85ed64
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-01-22 17:59:55 +0100

    feat: Create dummy data for user

---
+++ src/data/auth.js
@@ -0,0 +1,17 @@
+export const users = [
+    {
+        username: 'badru',
+        password: 'Badru@123',
+        role: 'admin'
+    },
+    {
+        username: 'musa',
+        password: 'Musa@123',
+        role: 'editor'
+    },
+    {
+        username: 'alli',
+        password: 'Alli@123',
+        role: 'user'
+    },
+];
